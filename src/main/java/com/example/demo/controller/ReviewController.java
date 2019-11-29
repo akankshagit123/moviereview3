3f86cd6b4beee1d08287e68e435a38ffd3da3ef9
ADMIN <chemburkar77@gmail.com>
2019-11-29 12:47:35 +0530

    As request WEBREQ2- 2 added Methods

--- src/main/java/com/example/demo/controller/ReviewController.java
+++ src/main/java/com/example/demo/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resources;
@@ -12,5 +13,10 @@ public class ReviewController {
     @GetMapping(value = "/message")
     public String getMessage() {
         return "hello world";
+    }
+
+    @GetMapping(value = "/message/{name}")
+    public String anotherMessage(@PathVariable("name") String name){
+        return "Hello"+name;
     }
 }
